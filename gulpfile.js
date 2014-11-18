3b1d1980aa6f7ee4a405eb48e9716d78e9268b61
Ryan Berckmans <ryanberckmans@gmail.com>
2014-11-18 17:34:45 -0500

    gulpfile: add 'coffee' task, omit *.coffee from 'client' task; update scripts/run-lighttpd to use the build/ directory

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,31 @@
 
 var gulp = require('gulp');
+var coffee = require('gulp-coffee');
 var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
 var del = require('del');
 
 var paths = {
-  client: 'client/*'
+  client: ['client/**/*', '!client/js/**/*.coffee'],
+  coffee: ['client/js/**/*.coffee'],
+  dest: 'build'
 }
 
 gulp.task('clean', function() {
-  del(['build']);
+  del([paths.dest]);
+});
+
+gulp.task('coffee', function() {
+  return gulp.src(paths.coffee)
+    .pipe(sourcemaps.init())
+      .pipe(coffee())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(paths.dest + '/js'));
 });
 
 gulp.task('client', function() {
-  return gulp.src(paths.client, { base: 'client'})
-    .pipe(gulp.dest('build'))
+  return gulp.src(paths.client, {base: 'client'})
+    .pipe(gulp.dest(paths.dest))
 });
 
-gulp.task('default', ['client']);
+gulp.task('default', ['coffee', 'client']);
