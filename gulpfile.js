e24ae91f7ffcd7953436018c8e3285e16e4c7b00
Ryan Berckmans <ryanberckmans@gmail.com>
2014-11-18 19:43:27 -0500

    gulpfile: add a 'test' task using gulp-mocha; package.json: add gulp-mocha

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,13 @@ var gulp = require('gulp');
 var coffee = require('gulp-coffee');
 var uglify = require('gulp-uglify');
 var sourcemaps = require('gulp-sourcemaps');
+var mocha = require('gulp-mocha');
 var del = require('del');
 
 var paths = {
   client: ['client/**/*', '!client/js/**/*.coffee'],
   coffee: ['client/js/**/*.coffee'],
+  test: ['client/test/**/*.coffee'],
   dest: 'build'
 }
 
@@ -15,7 +17,15 @@ gulp.task('clean', function() {
   del([paths.dest]);
 });
 
-gulp.task('coffee', function() {
+gulp.task('test', function() {
+  require('coffee-script/register');
+  return gulp.src(paths.test, { read:false })
+    .pipe(mocha({
+      reporter: 'nyan'
+    }));
+});
+
+gulp.task('coffee', ['test'], function() {
   return gulp.src(paths.coffee)
     .pipe(sourcemaps.init())
       .pipe(coffee())
@@ -24,8 +34,8 @@ gulp.task('coffee', function() {
 });
 
 gulp.task('client', function() {
-  return gulp.src(paths.client, {base: 'client'})
-    .pipe(gulp.dest(paths.dest))
+  return gulp.src(paths.client, { base: 'client' })
+    .pipe(gulp.dest(paths.dest));
 });
 
 gulp.task('default', ['coffee', 'client']);
