b906b7752cf354e5f72f84495d2786e50c31020f
Ryan Berckmans <ryanberckmans@gmail.com>
2014-11-21 23:59:37 -0500

    gulpfile.js: add coffee task (remove coffee from scripts task); watch task now correctly monitors coffee files; add gulp-mocha-phantomjs; add suite of test tasks: try 'gulp test' or 'gulp test:watch'

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 
 var gulp = require('gulp');
 var mainBowerFiles = require('main-bower-files');
+var mochaPhantomJS = require('gulp-mocha-phantomjs');
 
 // load plugins
 var $ = require('gulp-load-plugins')();
@@ -15,19 +16,24 @@ gulp.task('styles', function () {
         }))
         .pipe($.autoprefixer('last 1 version'))
         .pipe(gulp.dest('.tmp/styles'))
-        .pipe($.size());
+        .pipe($.size({title: 'styles'}));
+});
+
+gulp.task('coffee', function() {
+    return gulp.src('app/scripts/**/*.coffee')
+      .pipe($.coffee())
+      .pipe(gulp.dest('.tmp/scripts'))
+      .pipe($.size({title: 'compiled coffee'}));
 });
 
 gulp.task('scripts', function () {
-    return gulp.src('app/scripts/**/*.coffee')
-        .pipe($.coffee())
-        .pipe(gulp.src('app/scripts/**/*.js'))
+    return gulp.src('app/scripts/**/*.js')
         .pipe($.jshint())
         .pipe($.jshint.reporter(require('jshint-stylish')))
-        .pipe($.size());
+        .pipe($.size({title: 'js'}));
 });
 
-gulp.task('html', ['styles', 'scripts'], function () {
+gulp.task('html', ['styles', 'scripts', 'coffee'], function () {
     var jsFilter = $.filter('**/*.js');
     var cssFilter = $.filter('**/*.css');
 
@@ -42,7 +48,7 @@ gulp.task('html', ['styles', 'scripts'], function () {
         .pipe($.useref.restore())
         .pipe($.useref())
         .pipe(gulp.dest('dist'))
-        .pipe($.size());
+        .pipe($.size({title: 'html'}));
 });
 
 gulp.task('images', function () {
@@ -53,7 +59,7 @@ gulp.task('images', function () {
             interlaced: true
         })))
         .pipe(gulp.dest('dist/images'))
-        .pipe($.size());
+        .pipe($.size({title: 'images'}));
 });
 
 // mainBowerFiles() isn't compatible with an additional gulp.src().
@@ -105,7 +111,7 @@ gulp.task('connect', function () {
         });
 });
 
-gulp.task('serve', ['connect', 'styles'], function () {
+gulp.task('serve', ['connect', 'styles', 'coffee'], function () {
     require('opn')('http://localhost:9000');
 });
 
@@ -134,8 +140,7 @@ gulp.task('watch', ['connect', 'serve'], function () {
     gulp.watch([
         'app/*.html',
         '.tmp/styles/**/*.css',
-        'app/scripts/**/*.js',
-        'app/scripts/**/*.coffee',
+        '{.tmp,app}/scripts/**/*.js',
         'app/images/**/*'
     ]).on('change', function (file) {
         server.changed(file.path);
@@ -143,6 +148,80 @@ gulp.task('watch', ['connect', 'serve'], function () {
 
     gulp.watch('app/styles/**/*.scss', ['styles']);
     gulp.watch('app/scripts/**/*.js', ['scripts']);
+    gulp.watch('app/scripts/**/*.coffee', ['coffee']);
     gulp.watch('app/images/**/*', ['images']);
     gulp.watch('bower.json', ['wiredep']);
+});
+
+
+// ************************
+// Test Tasks below here
+// ************************
+
+// What's going on with this test stategy?
+// I would like to run my tests against the same code being deployed to production.
+// When running yeoman gulp-webapp, I was surprised to find that the test/ directory
+// is a mini website, complete with a separate bower.json for declaring test dependencies.
+// So, gulp-webapp (1) has a highly sophisticated build process for app/, and,
+//                 (2) has a mini website in test/ which itself has no tasks in this gulpfile.
+//
+// My test strategy is to duplicate the app/ build process for the test website,
+// and deploy the test website _on top_ of the regular build. Accomplish as so:
+// 
+// 1. build the regular website
+// 2. copy dist/ to test-dist/
+// 3. build test/ into test-dist/   (using .test-tmp/ for compiled coffeescript)
+// 4. run the tests using the final build in test-dist/
+
+gulp.task('test:clean', function () {
+    return gulp.src(['.test-tmp', 'test-dist'], { read: false }).pipe($.clean());
+});
+
+gulp.task('test:cp-dist', ['build', 'test:clean'], function() {
+  return gulp.src('dist/**/*')
+    .pipe(gulp.dest('test-dist'));
+});
+
+gulp.task('test:coffee', ['test:cp-dist'], function() {
+    return gulp.src('test/spec/**/*.coffee')
+      .pipe($.coffee())
+      .pipe(gulp.dest('.test-tmp/spec'));
+});
+
+gulp.task('test:scripts', ['test:cp-dist', 'test:coffee'], function () {
+    return gulp.src('test/spec/**/*.js')
+        .pipe($.jshint())
+        .pipe($.jshint.reporter(require('jshint-stylish')))
+});
+
+gulp.task('test:html', ['test:cp-dist', 'test:scripts'], function () {
+    return gulp.src('test/*.html')
+        .pipe($.useref.assets({searchPath: '{.test-tmp,test}'}))
+        .pipe($.useref.restore())
+        .pipe($.useref())
+        .pipe(gulp.dest('test-dist'))
+        .pipe($.size({title: 'test html'}));
+});
+
+gulp.task('test:build', ['test:html']);
+
+gulp.task('test', ['test:build'], function () {
+  return gulp.src('test-dist/index.html')
+      .pipe(mochaPhantomJS({
+          mocha: {},
+          reporter: 'min',
+          phantomjs: {
+            webSecurityEnabled: false
+          }
+      }))
+      .on('error', function() { this.emit('end'); }); // fail silently; mocha will print an error msg; and, an errored stream will blow up test:watch
+});
+
+gulp.task('test:watch', ['test'], function() {
+    // test:watch doesn't pick up dependency changes in bower.json or test/bower.json
+    gulp.watch([
+      'app/*.html',
+      'app/scripts/**/*.{js,coffee}',
+      'test/*.html',
+      'test/spec/**/*.{js,coffee}'], ['test']);
 });
