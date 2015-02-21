ff00185f4e5004f5f308d4e0c03bd5204488b0dd
Ryan Berckmans <ryanberckmans@gmail.com>
2015-02-21 12:30:23 -0500

    package.json, test/bower.json, update many dependencies. gulpfile.js, patch gulpfile with improvements generated by yeoman's generator-gulp-webapp 0.2.0.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 'use strict';
 // generated on 2014-11-21 using generator-gulp-webapp 0.1.0
+// updated on 2015-2-21 by manually comparing output of generator-gulp-webapp 0.2.0
+
+require('harmonize')();
 
 var gulp = require('gulp');
 var mainBowerFiles = require('main-bower-files');
@@ -9,44 +12,46 @@ var mochaPhantomJS = require('gulp-mocha-phantomjs');
 var $ = require('gulp-load-plugins')();
 
 gulp.task('styles', function () {
-    return gulp.src('app/styles/*.scss')
-        .pipe($.rubySass({
+    // gulp-ruby-sass doesn't support globs yet, only single files or directories
+    // https://github.com/sindresorhus/gulp-ruby-sass/tree/rw/1.0
+    return $.rubySass('app/styles/app.scss', {
             style: 'expanded',
-            precision: 10
-        }))
-        .pipe($.autoprefixer('last 1 version'))
+            precision: 10,
+            sourcemap: true, 
+        })
+        .on('error', function (err) { console.log(err.message); })
         .pipe(gulp.dest('.tmp/styles'))
+        .pipe($.sourcemaps.write())
         .pipe($.size({title: 'styles'}));
 });
 
 gulp.task('coffee', function() {
     return gulp.src('app/scripts/**/*.coffee')
+      .pipe($.sourcemaps.init())
       .pipe($.coffee())
+      .pipe($.sourcemaps.write())
       .pipe(gulp.dest('.tmp/scripts'))
       .pipe($.size({title: 'compiled coffee'}));
 });
 
-gulp.task('scripts', function () {
-    return gulp.src('app/scripts/**/*.js')
-        .pipe($.jshint())
-        .pipe($.jshint.reporter(require('jshint-stylish')))
-        .pipe($.size({title: 'js'}));
+gulp.task('jshint', function () {
+  return gulp.src('app/scripts/**/*.js')
+    .pipe($.jshint())
+    .pipe($.jshint.reporter('jshint-stylish'))
+    .pipe($.size({title: 'js'}));
 });
 
-gulp.task('html', ['styles', 'scripts', 'coffee'], function () {
-    var jsFilter = $.filter('**/*.js');
-    var cssFilter = $.filter('**/*.css');
+gulp.task('html', ['styles', 'jshint', 'coffee'], function () {
+    var assets = $.useref.assets({searchPath: '{.tmp,app}'});
 
     return gulp.src('app/*.html')
-        .pipe($.useref.assets({searchPath: '{.tmp,app}'}))
-        .pipe(jsFilter)
-        .pipe($.uglify())
-        .pipe(jsFilter.restore())
-        .pipe(cssFilter)
-        .pipe($.csso())
-        .pipe(cssFilter.restore())
-        .pipe($.useref.restore())
+        .pipe(assets)
+        // Right now, "gulp test" triggers a full build which can be slow:
+        // time 5s, comment for -90% .pipe($.if('*.js', $.uglify()))
+        // time 300ms, comment for additional -50% .pipe($.if('*.css', $.csso()))
+        .pipe(assets.restore())
         .pipe($.useref())
+        // time 150ms, comment for additional -33% .pipe($.if('*.html', $.minifyHtml({conditionals: true, loose: true})))
         .pipe(gulp.dest('dist'))
         .pipe($.size({title: 'html'}));
 });
@@ -54,7 +59,6 @@ gulp.task('html', ['styles', 'scripts', 'coffee'], function () {
 gulp.task('images', function () {
     return gulp.src('app/images/**/*')
         .pipe($.cache($.imagemin({
-            optimizationLevel: 3,
             progressive: true,
             interlaced: true
         })))
@@ -62,33 +66,26 @@ gulp.task('images', function () {
         .pipe($.size({title: 'images'}));
 });
 
-// mainBowerFiles() isn't compatible with an additional gulp.src().
-// ie gulp.src(mainBowerFiles()).pipe(gulp.src(...)) doesn't emit the total list of files as you'd expect.
-// That's why fonts:bower and fonts:app exist, instead of being one fonts task.
-gulp.task('fonts:bower', function () {
-    return gulp.src(mainBowerFiles())
-        .pipe($.filter('**/*.{eot,svg,ttf,woff}'))
-        .pipe($.flatten())
-        .pipe(gulp.dest('dist/fonts'))
-        .pipe($.size({title: 'bower fonts'}));
+gulp.task('fonts', function () {
+  return gulp.src(require('main-bower-files')().concat('app/fonts/**/*'))
+    .pipe($.filter('**/*.{eot,svg,ttf,woff}'))
+    .pipe($.flatten())
+    .pipe(gulp.dest('dist/fonts'))
+    .pipe($.size({title: 'fonts'}));
 });
-
-gulp.task('fonts:app', function () {
-    return gulp.src('app/fonts/**/*.{eot,svg,ttf,woff}')
-        .pipe(gulp.dest('dist/fonts'))
-        .pipe($.size({title: 'app fonts'}));
-});
-
-gulp.task('fonts', ['fonts:bower', 'fonts:app']);
 
 gulp.task('extras', function () {
-    return gulp.src(['app/*.*', '!app/*.html'], { dot: true })
-        .pipe(gulp.dest('dist'));
+    return gulp.src([
+        'app/*.*',
+        '!app/*.html',
+        'node_modules/apache-server-configs/dist/.htaccess'
+    ], {
+        dot: true
+    }).pipe(gulp.dest('dist'))
+    .pipe($.size({title: 'extras'}));
 });
 
-gulp.task('clean', function () {
-    return gulp.src(['.tmp', 'dist'], { read: false }).pipe($.clean());
-});
+gulp.task('clean', require('del').bind(null, ['.tmp', 'dist']));
 
 gulp.task('build', ['html', 'images', 'fonts', 'extras']);
 
@@ -96,63 +93,57 @@ gulp.task('default', ['clean'], function () {
     gulp.start('build');
 });
 
-gulp.task('connect', function () {
-    var connect = require('connect');
-    var app = connect()
-        .use(require('connect-livereload')({ port: 35729 }))
-        .use(connect.static('app'))
-        .use(connect.static('.tmp'))
-        .use(connect.directory('app'));
+gulp.task('connect', ['styles'], function () {
+  var serveStatic = require('serve-static');
+  var serveIndex = require('serve-index');
+  var app = require('connect')()
+    .use(require('connect-livereload')({port: 35729}))
+    .use(serveStatic('.tmp'))
+    .use(serveStatic('app'))
+    // paths to bower_components should be relative to the current file
+    // e.g. in app/index.html you should use ../bower_components
+    .use('/bower_components', serveStatic('bower_components'))
+    .use(serveIndex('app'));
 
-    require('http').createServer(app)
-        .listen(9000)
-        .on('listening', function () {
-            console.log('Started connect web server on http://localhost:9000');
-        });
+  require('http').createServer(app)
+    .listen(9000)
+    .on('listening', function () {
+      console.log('Started connect web server on http://localhost:9000');
+    });
 });
 
-gulp.task('serve', ['connect', 'styles', 'coffee'], function () {
+gulp.task('serve', ['connect', 'watch', 'coffee'], function () {
     require('opn')('http://localhost:9000');
 });
 
 // inject bower components
 gulp.task('wiredep', function () {
-    var wiredep = require('wiredep').stream;
+  var wiredep = require('wiredep').stream;
 
-    gulp.src('app/styles/*.scss')
-        .pipe(wiredep({
-            directory: 'app/bower_components'
-        }))
-        .pipe(gulp.dest('app/styles'));
+  gulp.src('app/styles/*.scss')
+    .pipe(wiredep())
+    .pipe(gulp.dest('app/styles'));
 
-    gulp.src('app/*.html')
-        .pipe(wiredep({
-            directory: 'app/bower_components'
-        }))
-        .pipe(gulp.dest('app'));
+  gulp.src('app/*.html')
+    .pipe(wiredep())
+    .pipe(gulp.dest('app'));
 });
 
-gulp.task('watch', ['connect', 'serve'], function () {
-    var server = $.livereload();
+gulp.task('watch', ['connect'], function () {
+    $.livereload.listen();
 
     // watch for changes
-
     gulp.watch([
         'app/*.html',
-        '.tmp/styles/**/*.css',
+        '{.tmp,app}/styles/**/*.css',
         '{.tmp,app}/scripts/**/*.js',
         'app/images/**/*'
-    ]).on('change', function (file) {
-        server.changed(file.path);
-    });
+    ]).on('change', $.livereload.changed);
 
-    gulp.watch('app/styles/**/*.scss', ['styles']);
-    gulp.watch('app/scripts/**/*.js', ['scripts']);
     gulp.watch('app/scripts/**/*.coffee', ['coffee']);
-    gulp.watch('app/images/**/*', ['images']);
+    gulp.watch('app/styles/**/*.scss', ['styles']);
     gulp.watch('bower.json', ['wiredep']);
 });
-
 
 // ************************
 // Test Tasks below here
@@ -173,31 +164,38 @@ gulp.task('watch', ['connect', 'serve'], function () {
 // 3. build test/ into test-dist/   (using .test-tmp/ for compiled coffeescript)
 // 4. run the tests using the final build in test-dist/
 
-gulp.task('test:clean', function () {
-    return gulp.src(['.test-tmp', 'test-dist'], { read: false }).pipe($.clean());
-});
+gulp.task('test:clean', require('del').bind(null, ['.test-tmp', 'test-dist']));
 
 gulp.task('test:cp-dist', ['build', 'test:clean'], function() {
   return gulp.src('dist/**/*')
     .pipe(gulp.dest('test-dist'));
 });
 
-gulp.task('test:coffee', ['test:cp-dist'], function() {
+// test:coffee shouldn't depend on test:clean,
+// but does so to prevent a race condition where test:clean errors
+// due to test:coffee.dest() preventing del from rmdir
+gulp.task('test:coffee', ['test:clean'], function() {
     return gulp.src('test/spec/**/*.coffee')
+      .pipe($.sourcemaps.init())
       .pipe($.coffee())
-      .pipe(gulp.dest('.test-tmp/spec'));
+      .pipe($.sourcemaps.write())
+      .pipe(gulp.dest('.test-tmp/spec'))
+      .pipe($.size({title: 'compiled test coffee'}));
 });
 
-gulp.task('test:scripts', ['test:cp-dist', 'test:coffee'], function () {
+gulp.task('test:jshint', function () {
     return gulp.src('test/spec/**/*.js')
         .pipe($.jshint())
         .pipe($.jshint.reporter(require('jshint-stylish')))
+        .pipe($.size({title: 'test js'}));
 });
 
-gulp.task('test:html', ['test:cp-dist', 'test:scripts'], function () {
+gulp.task('test:html', ['test:cp-dist', 'test:jshint', 'test:coffee'], function () {
+    var assets = $.useref.assets({searchPath: '{.test-tmp,test}'});
+
     return gulp.src('test/*.html')
-        .pipe($.useref.assets({searchPath: '{.test-tmp,test}'}))
-        .pipe($.useref.restore())
+        .pipe(assets)
+        .pipe(assets.restore())
         .pipe($.useref())
         .pipe(gulp.dest('test-dist'))
         .pipe($.size({title: 'test html'}));
@@ -209,7 +207,10 @@ gulp.task('test', ['test:build'], function () {
   return gulp.src('test-dist/index.html')
       .pipe(mochaPhantomJS({
           mocha: {},
-          reporter: 'min',
+          // reporters: find . | grep mocha.*reporter
+          // reports that blow up: nyan :<
+          // 
+          reporter: 'nyan',
           phantomjs: {
             webSecurityEnabled: false
           }
@@ -220,8 +221,9 @@ gulp.task('test', ['test:build'], function () {
 gulp.task('test:watch', ['test'], function() {
     // test:watch doesn't pick up dependency changes in bower.json or test/bower.json
     gulp.watch([
-      'app/*.html',
       'app/scripts/**/*.{js,coffee}',
       'test/*.html',
       'test/spec/**/*.{js,coffee}'], ['test']);
 });
+
+gulp.task('tw' ['test:watch']);
